3a3d8918fc05a2aaf04076f128f96a2e7b69a0af
jose.afjunior4 <juninho15feliz@gmail.com>
2020-03-05 11:55:36 -0300

    Initial commit

---
+++ app/src/main/java/br/com/sp/senac/applogin/EsqueceuActivity.java
@@ -0,0 +1,14 @@
+package br.com.sp.senac.applogin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EsqueceuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.esqueceu_layout);
+    }
+}
